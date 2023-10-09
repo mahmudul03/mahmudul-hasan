aac63e6c233164fe81172e9377f2a4ab8b1ba22e
mahmudul03 <97116205+mahmudul03@users.noreply.github.com>
2023-10-10 01:47:16 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+ var typed = new Typed (".multiple-text",{
+    strings: ["Forntent Devoloper",
+    "youtuber","bloggor","GraphiDesingner","UX/UIdevoloper","WebDesign"],
+    typedSpeed:100,
+    backSpeed:100,
+    backDeley:1000,
+    loop:true
+ })
+ 
